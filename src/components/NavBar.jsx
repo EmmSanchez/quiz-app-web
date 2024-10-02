cb46772d50c8b568c26d9d90753fa47bc41376cb
Emmanuel Sánchez <emmsanchezaranda@hotmail.com>
2024-10-02 02:52:24 -0600

    NavBar finished

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,34 @@
+import { AwardIcon, GitHubIcon, HomeIcon  } from "../assets/icons/Icons"
+
+export function NavBar() {
+  return (
+    <>
+      <nav className="w-dvw flex justify-center">
+        <div className="w-full max-w-7xl">
+          <div className="flex flex-row justify-between items-center p-3">
+            <a href="https://quiz-app-web-plum.vercel.app/" target="_blank" className="flex items-end gap-2 text-2xl font-semibold transition hover:cursor-pointer hover:opacity-85">
+              <AwardIcon className="bg-gradient-to-bl from-neutral-800 to-neutral-950 rounded-md p-1 size-8"/>
+              Quode
+                <span className="text-lg text-zinc-500 font-medium">
+                  Test, Learn, Code!
+                </span>
+            </a>
+
+            <ul className="flex items-center gap-1">
+              <li className="p-2 rounded-md hover:bg-zinc-800 hover:cursor-pointer">
+                <a href="/">
+                  <HomeIcon className="size-6"/>
+                </a>
+              </li>
+              <li className="p-2 rounded-md hover:bg-zinc-800 hover:cursor-pointer">
+                <a href="https://github.com/EmmSanchez/quiz-app-web" target="_blank">
+                  <GitHubIcon className="size-6"/>
+                </a>               
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
