6c1d4da69f981f7bc0d5320e636d0049f94150f7
Emmanuel Sánchez <emmsanchezaranda@hotmail.com>
2024-10-02 02:50:49 -0600

    Adding tailwind, animation to gradient and plugin for svgr

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+				gradient: {
+					"0%": {backgroundPosition: "0% 50%"},
+					"100%": {backgroundPosition: "90% 50%"}
+				}
+			},
+			animation: {
+				gradient: "gradient 4s linear infinite"
+			}
+    },
   },
   plugins: [],
 }
