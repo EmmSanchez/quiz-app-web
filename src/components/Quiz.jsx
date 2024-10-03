5fcbb15e03084cd03fc67eb591361461329c6d66
Emmanuel Sánchez <emmsanchezaranda@hotmail.com>
2024-10-03 01:48:16 -0600

    Show right answer when time is expired

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -10,7 +10,7 @@ export function Quiz({user, setIsSubmitted}) {
   const [wrongCounter, setWrongCounter] = useState(0)
   const [isNotChecked, setIsNotChecked] = useState(true)
   const [selectedAnswer, setSelectedAnswer] = useState(null)
-  const [countdown, setCountdown] = useState(1800)
+  const [countdown, setCountdown] = useState(15)
   const [timeLeft, setTimeLeft] = useState(0)
   const timerId = useRef()
 
@@ -135,8 +135,8 @@ export function Quiz({user, setIsSubmitted}) {
     setSelectedAnswer(null)
   }
 
-  const handleCheckAnswer = (e) => {
-    const choiced = e.target.textContent;
+  const handleCheckAnswer = (res) => {
+    const choiced = res;
     setSelectedAnswer(choiced)
 
     if (filteredQuestions[iterator].respuesta_correcta === choiced) {
@@ -153,7 +153,7 @@ export function Quiz({user, setIsSubmitted}) {
   useEffect(() => {
     setIsNotChecked(true)
 
-    setCountdown(1800);
+    setCountdown(15);
     
   },[iterator])
 
@@ -166,7 +166,7 @@ export function Quiz({user, setIsSubmitted}) {
     setWrongCounter(0);
     setIsNotChecked(true);
     setSelectedAnswer(null);
-    setCountdown(1800);
+    setCountdown(15);
     setIsFinished(false);
     setTimeLeft(0)
 
@@ -185,7 +185,7 @@ export function Quiz({user, setIsSubmitted}) {
   
 
   return (
-    <div className='quiz_container'>
+    <div className='flex flex-col justify-center items-center w-[500px] h-[600px] px-6 py-8 text-black bg-white rounded-lg'>
       <h1 className='text-5xl font-bold mb-4'>MN Quiz</h1>
       {
         isFinished ? 
@@ -195,15 +195,15 @@ export function Quiz({user, setIsSubmitted}) {
               <h1 className='user'>
                 {user}
               </h1>
-              <p>Tu puntaje es <span className='bold'>{timeLeft * rightCounter}</span></p>
+              <p>Tu puntaje es <span className='font-bold'>{timeLeft * rightCounter}</span></p>
 
               <div className="score">
                 <div className="right">
-                  <p className='bold'>Correctas</p>
+                  <p className='font-bold'>Correctas</p>
                   <p className='counter right_counter'>{rightCounter}</p>
                 </div>
                 <div className="wrong">
-                  <p className='bold'>Incorrectas</p>
+                  <p className='font-bold'>Incorrectas</p>
                   <p className='counter wrong_counter'>{wrongCounter}</p>
                 </div>
               </div>
@@ -221,31 +221,37 @@ export function Quiz({user, setIsSubmitted}) {
         :
         (
           <>
-            <div className="quiz_info">
+            <div className="flex w-full justify-between items-center pb-2">
               <p>{iterator + 1}/6</p>
-              <div className="quiz_timer">
+              <div className="flex items-center gap-2 bg-zinc-200 px-4 py-2 rounded-full">
                 <TimerIcon/>
                 <p>{formatTime(countdown)}</p>  
               </div>
             </div>
-            <div className="quiz_question">
-              <p className="question bold">{filteredQuestions[iterator].pregunta}</p>
-              <div className="choices">
+            <div className="w-full py-4 border-t-[1px] border-gray-500">
+              <p className="text-base font-bold">{filteredQuestions[iterator].pregunta}</p>
+              <div className="flex flex-col py-4 gap-2">
                 {
                   filteredQuestions[iterator].respuestas.map((res, index) => {
                     return (
                       <div key={index}>                                                                                  
-                        <button disabled={!isNotChecked} className={`choice ${!isNotChecked ? "choice-disabled" : ""} ${selectedAnswer ? ((selectedAnswer === res && selectedAnswer === filteredQuestions[iterator].respuesta_correcta) ? "right_answer" : (selectedAnswer === res ? "wrong_answer" : "")) : ""} ${!isNotChecked ? "choice-disabled" : ""} ${selectedAnswer ? ((selectedAnswer === res && selectedAnswer === filteredQuestions[iterator].respuesta_correcta) ? "right_answer" : (res === filteredQuestions[iterator].respuesta_correcta ? "right_answer" : "")) : ""}`} onClick={(e) => handleCheckAnswer(e)}>{res}</button>
+                        <button 
+                          disabled={!isNotChecked} 
+                          className={`choice ${!isNotChecked ? (res === filteredQuestions[iterator].respuesta_correcta ? 'choice-disabled pointer-events-none right_answer' : 'choice-disabled pointer-events-none') : ""} ${selectedAnswer ? ((selectedAnswer === res && selectedAnswer === filteredQuestions[iterator].respuesta_correcta) ? "right_answer" : (selectedAnswer === res ? "wrong_answer" : "")) : ""} ${!isNotChecked ? "choice-disabled" : ""} ${selectedAnswer ? ((selectedAnswer === res && selectedAnswer === filteredQuestions[iterator].respuesta_correcta) ? "right_answer" : (res === filteredQuestions[iterator].respuesta_correcta ? "right_answer" : "")) : ""}`} 
+                          onClick={() => handleCheckAnswer(res)}
+                        >
+                          {res}
+                        </button>
                       </div>
                     )
                   })
                 }
               </div>
             </div>
-            <div className="quiz_info">
+            <div className="flex w-full justify-between items-center pb-2">
               <div className="quiz_result">
-                <p className='bold'>✅ {rightCounter}</p>
-                <p className='bold'>❌ {wrongCounter}</p>
+                <p className='font-bold'>✅ {rightCounter}</p>
+                <p className='font-bold'>❌ {wrongCounter}</p>
               </div>
               <button disabled={isNotChecked} className={`next-button ${isNotChecked ? "disabled": ""}`} onClick={() => handleNextQuestion()}>Siguiente</button>
             </div>
